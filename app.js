a13ea903d531778fda2ec0f8d82fa852c96975a4
kyle67 <daniels.kyle@gmail.com>
2021-05-31 09:50:15 -0400

    added static public folder

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const request = require("request")
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+      
+    res.sendFile(__dirname + "/signup.html");
+
+})
+
+
+
 
 app.listen(3000,(req,res)=>{
 
